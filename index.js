1139d99f9251e8313a5474fdfc95992a9cb0f2d5
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-13 00:03:33 +0400

    [fix] - how to do it

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var mwcKernel       = require('mwc_kernel')
   , pluginSocketIo  = require('mwc_plugin_socket_io')
   , pluginChat      = require('mwc_plugin_chat');
+
 //setting up the config
-// var MWC = new mwcCore(require('./config.json').development);
 var mwc = mwcKernel(require('./config.json')[(process.env.NODE_ENV) ? (process.env.NODE_ENV) : 'development']);
 
 
@@ -10,17 +10,29 @@ mwc.usePlugin(pluginSocketIo);
 mwc.usePlugin(pluginChat);
 
 //extend application like mwc_core example
-//injecting plugin as an object
-mwc.usePlugin({
-  'extendCore': null, //can be ommited
-  'setAppParameters': null, //can be ommited
-  'setAppMiddlewares': null, //can be ommited
-  'extendAppRoutes': function (core) {
-    core.app.get('/newPlugin', function (req, res) {
-      res.send('New plugin is installed as object');
-    });
-  }
-});
+// vvv bad, syntax is outdated
+////injecting plugin as an object
+//mwc.usePlugin({
+//  'extendCore': null, //can be ommited
+//  'setAppParameters': null, //can be ommited
+//  'setAppMiddlewares': null, //can be ommited
+//  'extendAppRoutes': function (core) {
+//    core.app.get('/newPlugin', function (req, res) {
+//      res.send('New plugin is installed as object');
+//    });
+//  }
+//});
+
+/// ^^^ bad
+
+//it is better to use extendingFunctions, they have more possibilities than one called from plugin
+
+//mwc.extendApp
+//mwc.extendMiddleware
+//mwc.extendRoutes
+
+//this will help http://ci.monimus.com/docs/#/api
+
 
 
 mwc.start('app');//prepare application
